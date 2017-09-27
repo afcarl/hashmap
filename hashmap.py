59d9d6ec7ef92bac54d2eaf7146c4da85397830d
Divyansh Jha <divyanshj.16@gmail.com>
2017-09-27 14:04:44 +0530

    remove bug as hashmap was setting two values to same key while quadratic probing add new tests

--- hashmap.py
+++ hashmap.py
@@ -1,12 +1,30 @@
 class HashMap(object):
-	"""docstring for HashMap"""
+	"""
+		A hash table class, which can map key strings to values. This can have any size
+	    as long as the memory supports. In case of perfect hashing insertion and retrieval
+	    using keys is O(1).
 
-	def __init__(self, size,probing='linear'):
+	    This class have 2 types of probing i.e linear and quadratic. Linear probing causes
+	    clustering thus increasing the chances of collision, whereas quadratic clustering 
+	    does not cause clustering and hence less chances of clustering.
+
+	    However in quadratic probing there is a disdvantage. It is possible that all the
+	    empty spaces are not searched, so there are two conditions which the table size of
+	    quadratic probing must follow in order to ensure hitting of every empty space.
+	    1. Be a prime number
+	    2. never be more than half full 
+	    src = 'https://cathyatseneca.gitbooks.io/data-structures-and-algorithms/tables/quadratic_probing_and_double_hashing.html'
+	"""
+
+	def __init__(self, size,probing='quadratic',verbose=True):
+		if probing not in ['linear','quadratic']:
+			raise ValueError('probing can only be linear or quadratic not %s'%probing)
 		self.size = size
 		self.probing = probing
 		self.ctr = 1
 		self.values = [None] * self.size
 		self.keys = [None] * self.size
+		self.verbose = verbose
 
 	def reset(self):
 		self.ctr = 1
@@ -24,63 +42,83 @@ class HashMap(object):
 
 	def probe(self,old_idx):
 		if self.probing == 'linear':
-			self.ctr = 1
-			new_idx = (old_idx + self.ctr) % self.size 
+			new_idx = (old_idx + self.ctr) % self.size
+			self.ctr += 1
 		if self.probing == 'quadratic':
 			new_idx = (old_idx + self.ctr ** 2) % self.size
 			self.ctr += 1
 		return new_idx
 
-	def set(self,key,value,verbose=True):
-		if len([x for x in self.keys if x is not None]) == self.size:
-			if verbose:
-				print('Overflow!')
-			return False
+	def set(self,key,value):
+		if self.probing == 'quadratic':
+			if self.load() > 0.46: 
+				if self.verbose:
+					print('Overflow!')
+				return False
+		if self.probing == 'linear':
+			if self.load() == 1: 
+				if self.verbose:
+					print('Overflow!')
+				return False
 		self.sanity_check_key(key)
 		_idx = self.hash_index(key)
+		if self.verbose:
+			print('for key=%s Hashed_index = %d'%(key,_idx))
 		if self.keys[_idx] == None or self.keys[_idx] == key:
+			if self.verbose:
+				print('set key=%s, value=%s, at _idx=%d'%(key,value,_idx))
 			self.keys[_idx] = key
 			self.values[_idx] =  value
 			return True
 		else:
 			while(True):
-				_idx = self.probe(_idx)
-				if self.keys[_idx] == None or self.keys[_idx] == key:
-					self.keys[_idx] = key
-					self.values[_idx] = value
+				if self.verbose:
+					print('%d already occupied, probing...'%(_idx,))
+				_temp_idx = self.probe(_idx)
+				if self.verbose:
+					print(_temp_idx)
+				if self.keys[_temp_idx] == None or self.keys[_temp_idx] == key:
+					if self.verbose:
+						print('set key=%s, value=%s, at _temp_idx=%d'%(key,value,_temp_idx))
+					self.keys[_temp_idx] = key
+					self.values[_temp_idx] = value
+					self.ctr = 1
 					return True
 
 
-	def get(self,key):
+	def get(self,key,return_idx = False):
 		if key in self.keys:
 			self.sanity_check_key(key)
 			_idx = self.hash_index(key)
+			if self.verbose:
+				print('key=%s, hash_index=%d'%(key,_idx))
 			if self.keys[_idx] == key:
-				return self.values[_idx]
+				if self.verbose:
+					print("(without probing) key = %s found at %d with value %s"%(key,_idx,self.values[_idx]))
+				if return_idx:
+					return self.values[_idx],_idx
+				else:
+					return self.values[_idx]
 			else:
 				while(True):
-					_idx = self.probe(_idx)
-					if self.keys[_idx] == key:
-						return self.values[_idx]
+					_temp_idx = self.probe(_idx)
+					if self.verbose:
+						print('key=%s probing %d to %d'%(key,_idx,_temp_idx))
+					if self.keys[_temp_idx] == key:
+						if  self.verbose:
+							print("(After probing)key = %s found at %d with value %s"%(key,_temp_idx,self.values[_temp_idx]))
+						self.ctr = 1
+						if return_idx:
+							return self.values[_temp_idx],_temp_idx
+						else:
+							return self.values[_temp_idx]
 		else:
-			raise KeyError("could'nt find key!")
+			raise KeyError("could'nt find key! '%s'"%key)
 
 	def delete(self,key):
-		if key in self.keys:
-			self.sanity_check_key(key)
-			_idx = self.hash_index(key)
-			if self.keys[_idx] == key:
-				self.keys[_idx] = None
-				self.values[_idx] = None
-			else:
-				while(True):
-					_idx = self.probe(_idx)
-					if self.keys[_idx] == key:
-						self.keys[_idx] = None
-						self.values[_idx] = None
-						return
-		else:
-			raise KeyError("could'nt find key!")
+		_,_idx = self.get(key,return_idx=True)
+		self.keys[_idx] = None
+		self.values[_idx] = None
 
 	def load(self):
 		not_none_values = sum(x is not None for x in self.values)
--- test_hashmap.py
+++ test_hashmap.py
@@ -8,8 +8,8 @@ class test_hashmap(unittest.TestCase):
 
 
     def setUp(self):
-        size = math.floor(random.random() * 100) + 15 # minimum size 15 to avoid coflicts with line 26
-        self._dict = HashMap(size)
+        size = math.floor(random.random() * 15) + 15 # minimum size 15 to avoid coflicts with line 26
+        self._dict = HashMap(size,probing='quadratic',verbose=False)
 
     def test_hash_index(self):
         idx = self._dict.hash_index('divyansh')
@@ -22,8 +22,10 @@ class test_hashmap(unittest.TestCase):
         self.assertRaises(ValueError,self._dict.sanity_check_key,('asd','asd')) # passed a tuple to raise exception
 
     def test_probe_linear(self):
+        self._dict.probing = 'linear'
         num = self._dict.probe(5)
         self.assertEqual(num,6)
+        self._dict.ctr = 1
         num = self._dict.probe(self._dict.size + 3)
         self.assertEqual(num,4)
 
@@ -37,7 +39,17 @@ class test_hashmap(unittest.TestCase):
         self.assertEqual(self._dict.probe(self._dict.size - 1),3)
 
 
-    def test_set(self):
+    def test_set_quadratic(self):
+        self.assertEqual(self._dict.set('animal','monkey'),True)
+        self.assertEqual(self._dict.set('animal','lion'),True)
+        self.assertRaises(ValueError,self._dict.set,23,'twenty_three')
+        self._dict.reset()
+        for i in range(math.floor(self._dict.size/2)):
+            self.assertEqual(self._dict.set('animal' + str(i),'monkey' + str(i)),True)
+        self.assertLessEqual(self._dict.load(),0.5)
+
+    def test_set_linear(self):
+        self._dict.probing = 'linear'
         self.assertEqual(self._dict.set('animal','monkey'),True)
         self.assertEqual(self._dict.set('animal','lion'),True)
         self.assertRaises(ValueError,self._dict.set,23,'twenty_three')
@@ -45,16 +57,37 @@ class test_hashmap(unittest.TestCase):
         for i in range(self._dict.size):
             self.assertEqual(self._dict.set('animal' + str(i),'monkey' + str(i)),True)
         self.assertEqual(self._dict.load(),1)
-        self.assertEqual(self._dict.set('one_more_key_to_cause_overflow','monkey_n',False),False)
+        self.assertEqual(self._dict.set('one_more_key_to_cause_overflow','monkey_n'),False)
 
 
-    def test_get(self):
-        for i in range(self._dict.size):
-            self._dict.set('animal' + str(i),'monkey' + str(i))
+    def test_get_quadratic(self):
+        self.probing = 'quadratic'
+        for i in range(math.floor(self._dict.size/2)):
+            self.assertEqual(self._dict.set('animal' + str(i),'monkey' + str(i)),True)
             self.assertEqual(self._dict.get('animal' + str(i)),'monkey' + str(i))
         self.assertRaises(KeyError,self._dict.get,'bird')
 
-    def test_delete(self):
+    def test_get_linear(self):
+        self._dict.probing = 'linear'
+        for i in range(self._dict.size):
+            self.assertEqual(self._dict.set('animal' + str(i),'monkey' + str(i)),True)
+        for i in reversed(range(self._dict.size)):
+            self.assertEqual(self._dict.get('animal' + str(i)),'monkey' + str(i))
+        self.assertRaises(KeyError,self._dict.get,'bird')
+
+    def test_delete_quadratic(self):
+        self._dict.probing = 'quadratic'
+        self._dict.set('animal','monkey')
+        self._dict.delete('animal')
+        for i in range(math.floor(self._dict.size/2)):
+            self.assertEqual(self._dict.set('animal' + str(i),'monkey' + str(i)),True)
+        for i in reversed(range(math.floor(self._dict.size/2))):
+            self.assertEqual(self._dict.delete('animal' + str(i)),None)
+            self.assertRaises(KeyError,self._dict.delete,'animal' + str(i))
+        self.assertRaises(KeyError,self._dict.get,'animal')
+
+    def test_delete_linear(self):
+        self._dict.probing = 'linear'
         self._dict.set('animal','monkey')
         self._dict.delete('animal')
         for i in range(self._dict.size):
@@ -64,13 +97,25 @@ class test_hashmap(unittest.TestCase):
             self.assertRaises(KeyError,self._dict.delete,'animal' + str(i))
         self.assertRaises(KeyError,self._dict.get,'animal')
 
-    def test_load(self):
+    def test_load_quadratic(self):
+        self.probing = 'quadratic'
+        itr = math.floor(random.random() * self._dict.size/2 + 5)
+        for i in range(itr):
+            random_key = ''.join(random.choices(string.ascii_uppercase + string.digits, k=15))
+            self._dict.set(random_key,'monkey' + str(i))
+        load_value = self._dict.load()
+        self.assertLessEqual(load_value,0.6)
+
+    def test_load_linear(self):
+        self.probing = 'linear'
         itr = math.floor(random.random() * self._dict.size + 5)
         for i in range(itr):
             random_key = ''.join(random.choices(string.ascii_uppercase + string.digits, k=15))
-            self._dict.set(random_key,'monkey' + str(i),verbose=False)
+            self._dict.set(random_key,'monkey' + str(i))
         load_value = self._dict.load()
         self.assertLessEqual(load_value,1)
+
+
 
 
 if __name__ == '__main__':
